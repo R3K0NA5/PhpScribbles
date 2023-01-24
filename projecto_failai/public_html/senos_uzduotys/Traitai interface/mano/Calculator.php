6a2faefa3d3aebab051dee74377d3926409f0944
R3K0NA5 <120364068+R3K0NA5@users.noreply.github.com>
2023-01-24 17:24:30 +0200

    5 commit

--- projecto_failai/public_html/senos_uzduotys/Traitai interface/mano/Calculator.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace senos_uzduotys\Traitai interface\mano;
-interface Calculator
-{
-    public function calculate($x);
-}
-
-//sukuriamas calculiatorius daromi skaiciavimai cia
--- projecto_failai/public_html/senos_uzduotys/Traitai interface/mano/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-use interface\mano\Main;
-include_once 'Main.php';
-
-Main::run();

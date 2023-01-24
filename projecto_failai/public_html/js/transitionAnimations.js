567ae55c02e155d4ce6b32f2f7f55061888f5ce9
R3K0NA5 <120364068+R3K0NA5@users.noreply.github.com>
2023-01-24 19:10:49 +0200

    Antradienio stiliaus taisymas

--- projecto_failai/public_html/js/transitionAnimations.js
+++ projecto_failai/public_html/js/transitionAnimations.js
@@ -1,12 +1,11 @@
 setTimeout(() => {
     const box = document.getElementsByTagName("header")[0];
-    box.style.display = 'flex';
-}, 3450);
-
-
-
+    box.style.display = 'none';
+}, 0);
+/*
 setTimeout(() => {
     const box = document.getElementsByClassName("logosStrandoriginal")[0];
     box.style.display = 'none';
 }, 3450);
+*/
 
